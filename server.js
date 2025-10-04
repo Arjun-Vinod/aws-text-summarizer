4a702edaff950c2d4c5b7008c606b9b0e145557f
Arjun-Vinod <arjun.vinodaj77@gmail.com>
2025-10-04 12:52:50 +0000

    Update

--- server.js
+++ server.js
@@ -54,5 +54,5 @@ app.post("/summarize", async (req, res) => {
 });
 
 // Start the server
-app.listen(3000, () => console.log("✅ Server running on port 3000"));
+app.listen(3000, () => console.log("Server running on port 3000"));
 
